8e690cf0cf44706a2d14a9ec4d601f45629f5cdb
Olof hagsand <olof@hagsand.se>
2021-08-04 14:25:37 +0200

    Added test for leafref create

---
+++ test/test_leafref_key.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+# Leafref test for a construct common in openconfig, see eg neighbor-address in
+# openconfig-bgp-neighbor.yang:
+#    list neighbor {
+#      key "neighbor-address";
+#      leaf neighbor-address {
+#        type leafref {
+#          path "../config/neighbor-address";
+#        }
+#      container config {
+#        leaf neighbor-address; <<--- Must be defined first
+#      }
+#    }
+# Try to implement the following in the auto-cli:
+#   if the target of a leafref key has no value or is empty, create it
+# 
+
+# Magic line must be first in script (see README.md)
+s="$_" ; . ./lib.sh || if [ "$s" = $0 ]; then exit 0; else return 0; fi
+
+APPNAME=example
+
+cfg=$dir/conf_yang.xml
+fyang=$dir/leafref.yang
+
+cat <<EOF > $cfg
+<clixon-config xmlns="http://clicon.org/config">
+  <CLICON_CONFIGFILE>$cfg</CLICON_CONFIGFILE>
+  <CLICON_YANG_DIR>$dir</CLICON_YANG_DIR>
+  <CLICON_YANG_DIR>/usr/local/share/clixon</CLICON_YANG_DIR>
+  <CLICON_YANG_DIR>$IETFRFC</CLICON_YANG_DIR>
+  <CLICON_YANG_MAIN_FILE>$fyang</CLICON_YANG_MAIN_FILE>
+  <CLICON_CLISPEC_DIR>/usr/local/lib/$APPNAME/clispec</CLICON_CLISPEC_DIR>
+  <CLICON_CLI_DIR>/usr/local/lib/$APPNAME/cli</CLICON_CLI_DIR>
+  <CLICON_CLI_MODE>$APPNAME</CLICON_CLI_MODE>
+  <CLICON_SOCK>/usr/local/var/$APPNAME/$APPNAME.sock</CLICON_SOCK>
+  <CLICON_BACKEND_PIDFILE>/usr/local/var/$APPNAME/$APPNAME.pidfile</CLICON_BACKEND_PIDFILE>
+  <CLICON_XMLDB_DIR>/usr/local/var/$APPNAME</CLICON_XMLDB_DIR>
+</clixon-config>
+EOF
+
+cat <<EOF > $fyang
+module example{
+    yang-version 1.1;
+    namespace "urn:example:clixon";
+    prefix ex;
+    list neighbor {
+      key "neighbor-address";
+      leaf neighbor-address {
+        type leafref {
+          path "../config/neighbor-address";
+        }
+      }
+      container config {
+        leaf neighbor-address {
+          type string;
+        }
+      }
+    }
+}
+EOF
+
+new "test params: -f $cfg"
+
+if [ $BE -ne 0 ]; then
+    new "kill old backend"
+    sudo clixon_backend -zf $cfg
+    if [ $? -ne 0 ]; then
+	err
+    fi
+    new "start backend  -s init -f $cfg"
+    start_backend -s init -f $cfg
+fi
+
+new "wait backend"
+wait_backend
+
+new "leafref set neigbor a"
+expecteof "$clixon_netconf -qf $cfg" 0 "$DEFAULTHELLO<rpc $DEFAULTNS><edit-config><target><candidate/></target><config>
+<neighbor xmlns=\"urn:example:clixon\"><neighbor-address>a</neighbor-address></neighbor>
+</config></edit-config></rpc>]]>]]>" "^<rpc-reply $DEFAULTNS><ok/></rpc-reply>]]>]]>$"
+
+new "leafref get config"
+expecteof "$clixon_netconf -qf $cfg" 0 "$DEFAULTHELLO<rpc $DEFAULTNS><get-config><source><candidate/></source></get-config></rpc>]]>]]>" "^<rpc-reply $DEFAULTNS><data><neighbor xmlns=\"urn:example:clixon\"><neighbor-address>a</neighbor-address><config><neighbor-address>a</neighbor-address></config></neighbor></data></rpc-reply>]]>]]>"
+
+new "validate ok"
+expecteof "$clixon_netconf -qf $cfg" 0 "$DEFAULTHELLO<rpc $DEFAULTNS><validate><source><candidate/></source></validate></rpc>]]>]]>" "^<rpc-reply $DEFAULTNS><ok/></rpc-reply>]]>]]>$"
+
+if [ $BE -ne 0 ]; then
+    new "Kill backend"
+    # Check if premature kill
+    pid=$(pgrep -u root -f clixon_backend)
+    if [ -z "$pid" ]; then
+	err "backend already dead"
+    fi
+    # kill backend
+    stop_backend -f $cfg
+fi
+
+rm -rf $dir
+
+new "endtest"
+endtest
